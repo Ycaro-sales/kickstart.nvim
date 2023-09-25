d78da633d31b1f6b07137fe71a60395976b7595d
Esper-kun <ycaro00287@gmail.com>
2023-09-25 19:17:26 -0300

    fix: removed dropdown themes for now

--- init.lua
+++ init.lua
@@ -307,9 +307,9 @@ require('telescope').setup {
 pcall(require('telescope').load_extension, 'fzf')
 
 -- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles.get_dropdown({}),
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles,
   { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers.get_dropdown({}),
+vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers,
   { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
@@ -319,17 +319,17 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
-vim.keymap.set('n', '<leader>f', require('telescope.builtin').git_files.get_dropdown({}),
+vim.keymap.set('n', '<leader>f', require('telescope.builtin').git_files,
   { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<c-p>', require('telescope.builtin').find_files.get_dropdown({}), { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags.get_dropdown({}), { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string.get_dropdown({}),
+vim.keymap.set('n', '<c-p>', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string,
   { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep.get_dropdown({}),
+vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep,
   { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics.get_dropdown({}),
+vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics,
   { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume.get_dropdown({}), { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
 
 -- Custom Keymaps
 vim.keymap.set("n", "<leader>u", "<Cmd>UndotreeToggle<CR>")
