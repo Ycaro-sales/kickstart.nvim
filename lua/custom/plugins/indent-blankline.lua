71d6626ca51b0a842dfcc58143ed778b9ebd6cff
ycaro <ycaro00287@gmail.com>
2023-11-28 09:31:17 -0300

    fixed indent blankline setup

--- lua/custom/plugins/indent-blankline.lua
+++ lua/custom/plugins/indent-blankline.lua
@@ -3,8 +3,9 @@ return {
 	'lukas-reineke/indent-blankline.nvim',
 	-- Enable `lukas-reineke/indent-blankline.nvim`
 	-- See `:help indent_blankline.txt`
+	main = "ibl",
 	config = function()
-		require("ibl").setup()
+		require "ibl".setup()
 	end,
 	opts = {
 		char = '┊',
