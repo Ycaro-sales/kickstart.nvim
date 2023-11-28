36768feefe4ffce3468daad8107508ce7c6a5296
ycaro-sales <ycaro00287@gmail.com>
2023-11-28 19:33:41 -0300

    added git signs keymaps

--- init.lua
+++ init.lua
@@ -135,10 +135,27 @@ require('lazy').setup({
         changedelete = { text = '~' },
       },
       on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
         vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
 
+        vim.keymap.set('n', '<leader>hs', gs.stage_hunk, { desc = '' })
+        vim.keymap.set('n', '<leader>hr', gs.reset_hunk, { desc = '' })
+        vim.keymap.set('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
+          { desc = '' })
+        vim.keymap.set('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
+          { desc = '' })
+        vim.keymap.set('n', '<leader>hS', gs.stage_buffer, { desc = '' })
+        vim.keymap.set('n', '<leader>hu', gs.undo_stage_hunk, { desc = '' })
+        vim.keymap.set('n', '<leader>hR', gs.reset_buffer, { desc = '' })
+        vim.keymap.set('n', '<leader>hp', gs.preview_hunk, { desc = '' })
+        vim.keymap.set('n', '<leader>hb', function() gs.blame_line { full = true } end, { desc = '' })
+        vim.keymap.set('n', '<leader>tb', gs.toggle_current_line_blame, { desc = '' })
+        vim.keymap.set('n', '<leader>hd', gs.diffthis, { desc = '' })
+        vim.keymap.set('n', '<leader>hD', function() gs.diffthis('~') end, { desc = '' })
+        vim.keymap.set('n', '<leader>td', gs.toggle_deleted, { desc = '' })
+        vim.keymap.set({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = '' })
+
         -- don't override the built-in and fugitive keymaps
-        local gs = package.loaded.gitsigns
         vim.keymap.set({ 'n', 'v' }, ']c', function()
           if vim.wo.diff then return ']c' end
           vim.schedule(function() gs.next_hunk() end)
