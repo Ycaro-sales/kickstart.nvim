3aa217e6793e91fc7e143d2d8266e56f01a530fe
ycaro-sales <ycaro00287@gmail.com>
2023-11-28 21:41:44 -0300

    added todo comment plugin

---
+++ lua/custom/plugins/todo-comments.lua
@@ -0,0 +1,22 @@
+return {
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+
+	config = function()
+		require("todo-comments").setup()
+		vim.keymap.set('n', '<leader>st', ':TodoTelescope<cr>', { desc = "Todo Telescope" })
+		vim.keymap.set('n', '<leader>xt', ':TodoTrouble<cr>', { desc = "Todo Trouble" })
+		vim.keymap.set("n", "]x", function()
+			require("todo-comments").jump_next()
+		end, { desc = "Next todo comment" })
+
+		vim.keymap.set("n", "[x", function()
+			require("todo-comments").jump_prev()
+		end, { desc = "Previous todo comment" })
+	end,
+}
