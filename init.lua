6a8b5ad6d1deb780ffa6e2717499b98294990a97
Ycaro Sales <ycaro00287@gmail.com>
2025-03-09 16:34:30 -0400

    Modified tabout and deleted other plugins

--- init.lua
+++ init.lua
@@ -994,11 +994,10 @@ require('lazy').setup({
   },
 
   require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   { import = 'custom.plugins' },
   { import = 'custom.plugins.tools' },
